6e69aa830d93716cca7d9e4089c1492f072b2573
verelex <sdg7771@mail.ru>
2022-03-27 00:35:47 +0500

    test2

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,4 +1,8 @@
 public class MainClass {
+    int class_number = 20;
+    public int getClassNumber(){
+        return class_number;
+    }
     public static int getLocalNumber(){
         return 14;
     }
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -6,4 +6,11 @@ public class MainClassTest {
     public void testGetLocalNumber(){
         Assert.assertEquals("метод возвращает не 14",14, MainClass.getLocalNumber());
     }
+    @Test
+    public void testGetClassNumber(){
+        MainClass mc = new MainClass();
+        int tmp = mc.getClassNumber();
+        System.out.println(tmp);
+        Assert.assertTrue("меньше или равно 45", tmp > 45);
+    }
 }
